danielorbach/influxdb-client-go

commit a9c1e378757965a5e5bc81b7b5a3f09cbbdeec97

feat: add Flush()

api/writeAPIBlocking.go
@@ -21,6 +21,7 @@ import (
 // to internal buffer. If length ot the buffer is equal to the batch-size (set in write.Options), the buffer is sent to the server
 // and the result of the operation is returned.
 // When a point is written to the buffer, nil error is always returned.
+// Flush() can be used to trigger sending of batch when it doesn't have the batch-size.
 //
 // Synchronous writing is intended to use for writing less frequent data, such as a weather sensing, or if there is a need to have explicit control of failed batches.
 
@@ -42,6 +43,8 @@ type WriteAPIBlocking interface {
 	// EnableBatching turns on implicit batching
 	// Batch size is controlled via write.Options
 	EnableBatching()
+	// Flush forces write of buffer if batching is enabled, even buffer doesn't have the batch-size.
+	Flush(ctx context.Context) error
 }
 
 // writeAPIBlocking implements WriteAPIBlocking interface
@@ -107,4 +110,15 @@ func (w *writeAPIBlocking) WritePoint(ctx context.Context, point ...*write.Point
 		return err
 	}
 	return w.write(ctx, line)
+}
+
+func (w *writeAPIBlocking) Flush(ctx context.Context) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.batching && len(w.batch) > 0 {
+		body := strings.Join(w.batch, "\n")
+		w.batch = w.batch[:0]
+		return w.service.WriteBatch(ctx, iwrite.NewBatch(body, w.writeOptions.MaxRetryTime()))
+	}
+	return nil
 }

api/writeAPIBlocking_test.go
@@ -146,4 +146,15 @@ func TestWriteBatchIng(t *testing.T) {
 			service.Close()
 		}
 	}
+
+	for i := 0; i < 4; i++ {
+		err := writeAPI.WriteRecord(context.Background(), lines[i])
+		require.Nil(t, err)
+	}
+	assert.Equal(t, 0, service.Requests())
+	require.Len(t, service.Lines(), 0)
+	err := writeAPI.Flush(context.Background())
+	require.Nil(t, err)
+	assert.Equal(t, 1, service.Requests())
+	require.Len(t, service.Lines(), 4)
 }

commit c65256151e4adf5f7f51d0f33a140f16ea29942f

Update writeAPIBlocking.go
Commit https://github.com/influxdata/influxdb-client-go/pull/350/commits/a9c1e378757965a5e5bc81b7b5a3f09cbbdeec97 introduced a bug, where a successful `Flush` returns a non-nil error because the internal function (`w.service.WriteBatch`) returns a concrete pointer and not an `error`.

api/writeAPIBlocking.go
@@ -119,7 +119,9 @@ func (w *writeAPIBlocking) flush(ctx context.Context) error {
 		body := strings.Join(w.batch, "\n")
 		w.batch = w.batch[:0]
 		b := iwrite.NewBatch(body, w.writeOptions.MaxRetryTime())
-		return w.service.WriteBatch(ctx, b)
+		if err:= w.service.WriteBatch(ctx, b); err != nil {
+			return err
+		}
 	}
 	return nil
 }